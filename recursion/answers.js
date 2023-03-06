cfe18eb19d9df95598fbc084811f9af7f97f7c15
yosheeeee <evantipin@edu.hse.com>
2023-03-06 18:41:00 +0500

    add recursion

--- recursion/answers.js
+++ recursion/answers.js
@@ -10,3 +10,37 @@ let sumTo = n => {
 
 console.log(sumTo(4)) //10
 
+// 2
+let factorial = n => n == 1? 1: n*factorial(n-1)
+
+console.log(factorial(5)) //120
+
+// 3
+let fib = n => n <= 1 ? n: fib(n-1)+ fib(n-2)
+
+console.log(fib(7)) //13
+
+// 4
+
+let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+
+let printList = list => {
+    console.log(list.value)
+    if (list.next != null){
+        printList(list.next)
+    }
+}
+
+printList(list)
